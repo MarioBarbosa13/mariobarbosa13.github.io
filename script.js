1e54f46372ba66d67badbc23663ccea7e279d591
Mário_Barbosa <josemariodelimabarbosajunior3@gmail.com>
2026-02-24 11:51:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    const cvEn = document.getElementById("cv-en");
+    const cvPt = document.getElementById("cv-pt");
+
+    // Padrão inicial (Inglês)
+    cvEn.style.display = "inline-block";
+    cvPt.style.display = "none";
+
     window.setLanguage = function(lang) {
 
         if(lang === "pt") {
+
             document.documentElement.lang = "pt";
 
             document.getElementById("subtitle").innerText =
             "Analista de Geoprocessamento | Sensoriamento Remoto | Análise Ambiental";
 
-            document.getElementById("about-title").innerText = "Sobre Mim";
-            document.getElementById("about-text").innerText =
-            "Analista em Geoprocessamento com experiência em sensoriamento remoto, monitoramento ambiental e análise espacial.";
-
-            document.getElementById("cv-btn").innerText = "Baixar Currículo";
-            document.getElementById("cv-btn").href = "pdf/jose-barbosa-cv-pt.pdf";
+            cvEn.style.display = "none";
+            cvPt.style.display = "inline-block";
         }
 
         if(lang === "en") {
+
             document.documentElement.lang = "en";
 
             document.getElementById("subtitle").innerText =
             "Geospatial Analyst | Remote Sensing | Environmental Analysis";
 
-            document.getElementById("about-title").innerText = "About Me";
-            document.getElementById("about-text").innerText =
-            "Geospatial Analyst with experience in remote sensing and spatial analysis.";
-
-            document.getElementById("cv-btn").innerText = "Download CV";
-            document.getElementById("cv-btn").href = "pdf/jose-barbosa-cv-en.pdf";
+            cvEn.style.display = "inline-block";
+            cvPt.style.display = "none";
         }
     }
 
