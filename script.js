4daae82b2a19cf824de55c6eee2b1b204932873c
Mário_Barbosa <josemariodelimabarbosajunior3@gmail.com>
2026-02-24 20:56:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -53,4 +53,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Começa em inglês
     setLanguage("en");
+}
+                          // ANIMAÇÃO AO ROLAR
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+    observer.observe(section);
 });
+});
+
