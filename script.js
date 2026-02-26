ab1cb671a11ca1b660d9471f9e853044a409e95a
Mário_Barbosa <josemariodelimabarbosajunior3@gmail.com>
2026-02-26 00:08:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -111,4 +111,25 @@ document.addEventListener("DOMContentLoaded", function() {
         observer.observe(section);
     });
 
+// ===== IMAGE EXPAND =====
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".project-card img").forEach(img => {
+    img.addEventListener("click", function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    });
 });
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+};
+
+modal.onclick = function(e) {
+    if(e.target === modal) {
+        modal.style.display = "none";
+    }
+};
